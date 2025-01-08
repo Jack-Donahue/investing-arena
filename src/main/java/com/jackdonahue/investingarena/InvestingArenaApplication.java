b6ad237291c22170c9362331b6075c89abf1e686
Jack-Donahue <jfdonahu@iu.edu>
2025-01-07 23:58:33 -0800

    Initial commit: Investing Arena Project Setup

---
+++ src/main/java/com/jackdonahue/investingarena/InvestingArenaApplication.java
@@ -0,0 +1,13 @@
+package com.jackdonahue.investingarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestingArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestingArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackdonahue/investingarena/InvestingArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackdonahue.investingarena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestingArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
